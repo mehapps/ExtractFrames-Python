1c0569c7e6425975397df0ee539b5b40a62c6b46
mehapps <63882957+mehapps@users.noreply.github.com>
2022-06-22 18:23:42 -0400

    Create ExtractFrames.py

---
+++ ExtractFrames.py
@@ -0,0 +1,18 @@
+if __name__ == "__main__":
+    print("Please do not run this as a script!")
+    exit()
+else:
+    def frames(dir_path, video_path):
+        import cv2
+        cam = cv2.VideoCapture(video_path)
+        for i in range:
+            ret,frame = cam.read()
+            if ret:
+                name = dir_path + str(i) + '.jpg'
+                print ('Creating ' + name)
+                cv2.imwrite(name, frame)
+                print("Created " + name)
+            else:
+                cam.release()
+                cv2.destroyAllWindows()
+                break
